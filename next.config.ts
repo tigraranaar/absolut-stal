69f65d3eaf9d3dd239ff5ee017bfd7b9006d25d3
Tigran Arakyan <tigran.arakyan@decript.tech>
2025-11-18 03:27:11 +0400

    feat: update Next.js configuration for trailing slashes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true, // Создавать папки с index.html вместо файлов .html
   images: {
     unoptimized: true,
   },
